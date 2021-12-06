18a7ab33f07269cc8597cd80929eae7673d427f9
LTBurton1 <Liam.burton@hotmail.com>
2021-12-06 17:12:48 +0000

    Part 4 Done

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-import myCollection from "./collection.js"
-
-// Part 2:
+import myCollection from "./collection.js";
+import chalk from "chalk";
 
 function describeItem(item) {
   if (item.count === 1) {
     console.log(
-      `I have a ${item.name}. Here's what I like about it: ${item.whatILike}`
+      `I have ${
+        chalk.yellow(1) + " " + chalk.cyan(item.name)
+      }. Here's what I like about it: ${chalk.green(item.whatILike)}`
     );
   } else
     console.log(
-      `I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}`
+      `I have ${chalk.yellow(item.count)} ${chalk.cyan(
+        item.name + "s"
+      )}. Here's what I like about them: ${chalk.green(item.whatILike)}.`
     );
 }
+
+// Message for Mark - I've changed it a bit so that it says "1" in yellow in the count === 1 messages rather than just "a" as that was coming back in blue by default which looked weird compared to the count > 1 messages.
 
 describeItem(myCollection[0]);
 
